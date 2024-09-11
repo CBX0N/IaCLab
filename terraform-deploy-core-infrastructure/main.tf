1b728420a7cb38dcd72e568fdec80ebb74405d76
charlie boon <charlie.boon@sky.com>
2024-09-11 15:01:31 +0100

    Adding K3s vm's

--- terraform-deploy-core-infrastructure/main.tf
+++ terraform-deploy-core-infrastructure/main.tf
@@ -5,13 +5,13 @@ module "bind9_vm" {
   pve_connection    = var.pve_connection
 }
 
-# module "k3s_vm" {
-#   for_each          = toset(["k3s0", "k3s1", "k3s2"])
-#   source            = "../terraform-modules/vm"
-#   default_vm_config = var.default_vm_config
-#   vm_config         = var.virtual_machines[each.key]
-#   pve_connection    = var.pve_connection
-# }
+module "k3s_vm" {
+  for_each          = toset(["k3s0", "k3s1", "k3s2"])
+  source            = "../terraform-modules/vm"
+  default_vm_config = var.default_vm_config
+  vm_config         = var.virtual_machines[each.key]
+  pve_connection    = var.pve_connection
+}
 
 module "haproxy_vm" {
   for_each          = toset(["haproxy1"])
