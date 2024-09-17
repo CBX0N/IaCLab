8e7540496b9263d03411102f51fb95330582038e
charlie boon <charlie.boon@sky.com>
2024-09-17 22:04:21 +0100

    ADDING LONGHORN RETAIN

--- terraform-modules/guacamole/pvc.tf
+++ terraform-modules/guacamole/pvc.tf
@@ -7,13 +7,12 @@ resource "kubernetes_persistent_volume_claim" "postgresql_pvc" {
     name = "postgresql-pvc"
   }
   spec {
-    storage_class_name = "longhorn"
+    storage_class_name = "longhorn-retain"
     access_modes       = ["ReadWriteOnce"]
     resources {
       requests = {
         storage = "1Gi"
       }
     }
-    # volume_name = kubernetes_persistent_volume.postgres_pv.metadata[0].name
   }
 }
